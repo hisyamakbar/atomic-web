c278bc6b34b2a47dd452f83666467250005eb687
Hisyam Akbar <akbar.hisyam1@gmail.com>
2024-07-22 17:27:37 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            keyframes: {
+                slide: {
+                    '0%': { transform: 'translateX(0%)' },
+                    '100%': { transform: 'translateX(-100%)' },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
